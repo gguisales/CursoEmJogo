2ffd520da707bd391557b6bc6736eaad34857619
GUISÃO The Legend <87276808+gguisales@users.noreply.github.com>
2022-05-11 10:41:52 -0300

    Add files via upload

---
+++ Curso em Jogo.sql
@@ -0,0 +1,6 @@
+SELECT * FROM jogo;
+
+INSERT INTO pergunta (nome, id_jogo) VALUES
+('Qual o nome verdadeiro do Gustavo Guanabira',40);
+INSERT INTO ALTERNATIVA (texto,resposta,id_pergunta) VALUES 
+()
